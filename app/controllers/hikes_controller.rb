5839b96c9e2dfe9e4ed2916eace0d7a8fb2b2d35
Alejandra Jimenez <axjimenez89@gmail.com>
2016-11-05 15:05:07 -0700

    deploying

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -4,7 +4,7 @@ def show
     if params["q"]
       city = params["q"]
       headers = {
-       "
+
          "Accept" => "text/plain"}
           results = HTTParty.get(
         #"https://trailapi-trailapi.p.mashape.com/?lat&limit=25&lon&q[activities_activity_name_cont]&q[city_cont]&q[country_cont]&q[state_cont]=#{params["q"]}",
